8e00360bf4d51470aa7b704825796c22f17890ec
habacker <habacker@c19a1394-0e18-0410-b06c-9b707b4e42b1>
2006-08-18 20:38:45 +0000

    minor doc fix
    git-svn-id: https://windbus.svn.sourceforge.net/svnroot/windbus/trunk@129 c19a1394-0e18-0410-b06c-9b707b4e42b1

--- dbus/dbus-sysdeps-util-unix.c
+++ dbus/dbus-sysdeps-util-unix.c
@@ -1,5 +1,5 @@
 /* -*- mode: C; c-file-style: "gnu" -*- */
-/* dbus-sysdeps-util-unix.c Would be in dbus-sysdeps.c, but not used in libdbus
+/* dbus-sysdeps-util-unix.c Would be in dbus-sysdeps-unix.c, but not used in libdbus
  * 
  * Copyright (C) 2002, 2003, 2004, 2005  Red Hat, Inc.
  * Copyright (C) 2003 CodeFactory AB
