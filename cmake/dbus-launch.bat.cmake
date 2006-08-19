f59c43558cec57539d3d07e1b0c36946756e3218
habacker <habacker@c19a1394-0e18-0410-b06c-9b707b4e42b1>
2006-08-19 16:01:25 +0000

    fixed some package generator issues
    git-svn-id: https://windbus.svn.sourceforge.net/svnroot/windbus/trunk@146 c19a1394-0e18-0410-b06c-9b707b4e42b1

--- cmake/dbus-launch.bat.cmake
+++ cmake/dbus-launch.bat.cmake
@@ -7,7 +7,7 @@ set DBUS_SESSION_BUS_ADDRESS=@DBUS_SESSION_BUS_DEFAULT_ADDRESS@
 :: system bus address
 set DBUS_SYSTEM_BUS_DEFAULT_ADDRESS=@DBUS_SYSTEM_BUS_DEFAULT_ADDRESS@ 
 
-if exist bin\dbus-daemon.exe (
+if exist bus\build-session.conf (
   @echo "starting local dbus daemon"
 	start bin\dbus-daemon --config-file=bus\build-session.conf
 ) else (
