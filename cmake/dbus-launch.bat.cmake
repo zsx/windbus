1651f6cf153f13fbcd4657f130bd9223767810f7
habacker <habacker@c19a1394-0e18-0410-b06c-9b707b4e42b1>
2006-08-19 14:17:44 +0000

    - renamed env.bat to dbus-env.bat - added dbus-launch.bat script
    git-svn-id: https://windbus.svn.sourceforge.net/svnroot/windbus/trunk@142 c19a1394-0e18-0410-b06c-9b707b4e42b1

---
+++ cmake/dbus-launch.bat.cmake
@@ -0,0 +1,10 @@
+:: environment setting for dbus clients
+@echo off
+
+:: session bus address
+set DBUS_SESSION_BUS_ADDRESS=@DBUS_SESSION_BUS_DEFAULT_ADDRESS@
+
+:: system bus address
+set DBUS_SYSTEM_BUS_DEFAULT_ADDRESS=@DBUS_SYSTEM_BUS_DEFAULT_ADDRESS@ 
+
+start dbus-daemon --session
