d1c2c2f4c661df328ba9da25137b90857a3c3665
habacker <habacker@c19a1394-0e18-0410-b06c-9b707b4e42b1>
2006-11-09 22:06:54 +0000

    - added DBUSDIR environment for setting specific dbus installation dir - changed dbus installation search order to local, %DBUSDIR% %ProgramFiles%\dbus - no need for build-xxx.conf files because relative pathes are supported now
    git-svn-id: https://windbus.svn.sourceforge.net/svnroot/windbus/trunk@377 c19a1394-0e18-0410-b06c-9b707b4e42b1

--- cmake/dbus-launch.bat.cmake
+++ cmake/dbus-launch.bat.cmake
@@ -8,9 +8,19 @@ set DBUS_SESSION_BUS_ADDRESS=@DBUS_SESSION_BUS_DEFAULT_ADDRESS@
 set DBUS_SYSTEM_BUS_DEFAULT_ADDRESS=@DBUS_SYSTEM_BUS_DEFAULT_ADDRESS@ 
 
 if exist bus\build-session.conf (
-  @echo "starting local dbus daemon"
-	start bin\dbus-daemon --config-file=bus\build-session.conf
+  @echo starting local dbus daemon
+	 start bin\dbus-daemon --config-file=bus\session.conf
 ) else (
-  @echo "starting global dbus daemon"
-	start c:/Programme/dbus/bin/dbus-daemon --session
+	if not "%DBUSDIR%"=="" (
+	  @echo starting dbus daemon identified by DBUSDIR=%DBUSDIR%
+		start %DBUSDIR%\bin\dbus-daemon --session
+		pause
+	) else (
+		if exist "%ProgramFiles%\dbus\bin\dbus-daemon.exe" (
+		  @echo starting global dbus daemon located in %ProgramFiles%\dbus
+			start %ProgramFiles%\dbus\bin\dbus-daemon --session
+		) else (
+	  	@echo please set DBUSDIR to your DBUS installation dir and restart this script
+		) 
+	)
 )
