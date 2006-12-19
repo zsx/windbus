3e70b9ccd0c3ceb5c551736c5e94d7c264d6e874
syntheticpp <syntheticpp@c19a1394-0e18-0410-b06c-9b707b4e42b1>
2006-12-19 19:59:59 +0000

    also work on a non German Windows, thanks to Gerard Braad Jr.
    git-svn-id: https://windbus.svn.sourceforge.net/svnroot/windbus/trunk@539 c19a1394-0e18-0410-b06c-9b707b4e42b1

--- cmake/dbus-launch.bat.cmake
+++ cmake/dbus-launch.bat.cmake
@@ -9,16 +9,16 @@ set DBUS_SYSTEM_BUS_DEFAULT_ADDRESS=@DBUS_SYSTEM_BUS_DEFAULT_ADDRESS@
 
 if exist bus\session.conf (
   @echo starting local dbus daemon
-	 start bin\dbus-daemon --config-file=bus\session.conf
+	 start "D-Bus daemon" "bin\dbus-daemon" "--config-file=bus\session.conf"
 ) else (
 	if not "%DBUSDIR%"=="" (
 	  @echo starting dbus daemon identified by DBUSDIR=%DBUSDIR%
-		start %DBUSDIR%\bin\dbus-daemon --session
+		start "D-Bus daemon" "%DBUSDIR%\bin\dbus-daemon" "--session"
 		pause
 	) else (
 		if exist "%ProgramFiles%\dbus\bin\dbus-daemon.exe" (
 		  @echo starting global dbus daemon located in %ProgramFiles%\dbus
-			start %ProgramFiles%\dbus\bin\dbus-daemon --session
+			start "D-Bus daemon" "%ProgramFiles%\dbus\bin\dbus-daemon" "--session"
 		) else (
 	  	@echo please set DBUSDIR to your DBUS installation dir and restart this script
 		) 
