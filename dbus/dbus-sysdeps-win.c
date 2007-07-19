f97720f3ab6a0b90f598997ae217402c072ec561
chehrlic <chehrlic@c19a1394-0e18-0410-b06c-9b707b4e42b1>
2007-07-19 18:01:24 +0000

    strcat_s is a bad idea when you want to support mingw and msvc < 8 ...
    git-svn-id: https://windbus.svn.sourceforge.net/svnroot/windbus/trunk@706 c19a1394-0e18-0410-b06c-9b707b4e42b1

--- dbus/dbus-sysdeps-win.c
+++ dbus/dbus-sysdeps-win.c
@@ -5592,12 +5592,15 @@ _dbus_get_config_file_name(DBusString *config_file, char *s)
 {
   char path[MAX_PATH*2];
   int path_size = sizeof(path);
+  int len = 4 + strlen(s);
 
   if (!_dbus_get_install_root(path,path_size))
     return FALSE;
 
-  strcat_s(path,path_size,"etc\\");
-  strcat_s(path,path_size,s);
+  if(len > sizeof(path)-2)
+    return FALSE;
+  strcat(path,"etc\\");
+  strcat(path,s);
   if (_dbus_file_exists(path)) 
     {
       // find path from executable 
@@ -5608,8 +5611,10 @@ _dbus_get_config_file_name(DBusString *config_file, char *s)
     {
       if (!_dbus_get_install_root(path,path_size))
         return FALSE;
-      strcat_s(path,path_size,"bus\\");
-      strcat_s(path,path_size,s);
+      if(len + strlen(path) > sizeof(path)-2)
+        return FALSE;
+      strcat(path,"bus\\");
+      strcat(path,s);
   
       if (_dbus_file_exists(path)) 
         {
